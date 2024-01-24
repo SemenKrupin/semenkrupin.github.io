cb47e128a57ad5ac1fca12ee13583ba7a7e315ba
Семен Крупин <semenkrupin@gmail.com>
2024-01-24 16:22:57 +0300

    2

---
+++ script.js
@@ -0,0 +1,318 @@
+const obj = {
+    elButton: null,
+    elWrapper: null,
+    scoreProgress: null,
+    energyScore: null
+}
+
+const ram = {
+    energyInterval: null,
+    energyValue: 1,
+    energyClickValue: -3
+}
+
+$.fn.flyTo = function (target, source, callback) {
+    var divider = 8;
+    $(source).animate({
+        opacity: 0.4,
+        left: $(target).offset().left + ($(target).width() / 2) - ($(source).width() / divider) / 2,
+        top: $(target).offset().top + 80
+    }, 600,
+        function () {
+            $(source).stop();
+            $(source).remove();
+            if (callback) callback();
+        });
+}
+
+function startIntervalEnergy() {
+    ram.energyInterval = setInterval(function () {
+        const value = parseInt(obj.energyScore.getAttribute('value')) + ram.energyValue;
+        if (value >= 0 && value <= parseInt(obj.energyScore.getAttribute('max'))) {
+            if (value == 0) {
+                ram.energyValue = 1;
+            }
+            obj.energyScore.setAttribute('value', value);
+            obj.energyScore.parentElement.getElementsByTagName('span')[0].innerText = value + '/' + obj.energyScore.getAttribute('max');
+        }
+    }, 1000);
+}
+
+function clearIntervalEnergy() {
+    if (ram.energyInterval) {
+        clearInterval(ram.energyInterval);
+        ram.energyInterval = null;
+    }
+}
+
+function changeEnergyValue(value, flagFn = () => { }) {
+    const currValue = parseInt(obj.energyScore.getAttribute('value'));
+    if (currValue + value >= 0 && currValue + value <= parseInt(obj.energyScore.getAttribute('max'))) {
+        obj.energyScore.setAttribute('value', currValue + value);
+        obj.energyScore.parentElement.getElementsByTagName('span')[0].innerText = currValue + value + '/' + obj.energyScore.getAttribute('max');
+        flagFn(true);
+    }
+    else if (currValue + value <= 0) {
+        obj.energyScore.setAttribute('value', '0');
+        obj.energyScore.parentElement.getElementsByTagName('span')[0].innerText = '0/' + obj.energyScore.getAttribute('max');
+        flagFn(false);
+    }
+    else if (currValue + value >= parseInt(obj.energyScore.getAttribute('max'))) {
+        obj.energyScore.setAttribute('value', obj.energyScore.getAttribute('max'));
+        obj.energyScore.parentElement.getElementsByTagName('span')[0].innerText = obj.energyScore.getAttribute('max') + '/' + obj.energyScore.getAttribute('max');
+        flagFn(false);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    Telegram.WebApp.expand();
+    obj.elButton = document.querySelector(".treat-button");
+    obj.elWrapper = document.querySelector(".treat-wrapper");
+    obj.elButton.addEventListener("click", addTreats);
+    obj.elButton.click();
+
+    obj.scoreProgress = document.querySelector(".scoreProgress");
+    obj.energyScore = document.querySelector(".energyScore");
+
+    window.addEventListener("resize", () => {
+        width = window.innerWidth;
+        height = window.innerHeight;
+    });
+    startIntervalEnergy();
+});
+
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+const body = document.body;
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const treatmojis = [
+    {
+        "v": { "score": -3 },
+        "i": "💩",
+        "n": "d"
+    },
+    {
+        "v": { "score": +5 },
+        "i": "🤑",
+        "n": "s"
+    },
+    {
+        "v": { "score": +3 },
+        "i": "🚀",
+        "n": "e"
+    }
+];
+const treats = [];
+const radius = 11;
+
+const Cd = 0.47;
+const rho = 1.22;
+const A = Math.PI * radius * radius / 10000;
+const ag = 9.81;
+const frameRate = 1 / 60;
+
+function createTreat() {
+    const vx = getRandomArbitrary(-10, 10);
+    const vy = getRandomArbitrary(-10, 1);
+
+    const item = treatmojis[getRandomInt(0, treatmojis.length - 1)];
+    const el = document.createElement("div");
+    el.className = "treat";
+    const inner = document.createElement("span");
+    inner.className = "inner";
+    inner.innerText = item.i;
+    el.setAttribute('score', item.v.score);
+    el.setAttribute('name', item.n);
+    el.appendChild(inner);
+
+    obj.elWrapper.appendChild(el);
+
+    const rect = el.getBoundingClientRect();
+
+    const lifetime = ram.energyClickValue * 1000 * -1; //getRandomArbitrary(2000, 3000);
+
+    el.style.setProperty("--lifetime", lifetime);
+
+    const treat = {
+        el,
+        absolutePosition: { x: rect.left, y: rect.top },
+        position: { x: rect.left, y: rect.top + 20 },
+        velocity: { x: vx, y: vy },
+        mass: 0.1, //kg
+        radius: el.offsetWidth, // 1px = 1cm
+        restitution: -.7,
+
+        lifetime,
+        direction: vx > 0 ? 1 : -1,
+
+        animating: true,
+
+        remove() {
+            this.animating = false;
+            this.el.parentNode.removeChild(this.el);
+        },
+
+        animate() {
+            const treat = this;
+            let Fx =
+                -0.5 *
+                Cd *
+                A *
+                rho *
+                treat.velocity.x *
+                treat.velocity.x *
+                treat.velocity.x /
+                Math.abs(treat.velocity.x);
+            let Fy =
+                -0.5 *
+                Cd *
+                A *
+                rho *
+                treat.velocity.y *
+                treat.velocity.y *
+                treat.velocity.y /
+                Math.abs(treat.velocity.y);
+
+            Fx = isNaN(Fx) ? 0 : Fx;
+            Fy = isNaN(Fy) ? 0 : Fy;
+
+            // Calculate acceleration ( F = ma )
+            var ax = Fx / treat.mass;
+            var ay = ag + Fy / treat.mass;
+            // Integrate to get velocity
+            treat.velocity.x += ax * frameRate;
+            treat.velocity.y += ay * frameRate;
+
+            // Integrate to get position
+            treat.position.x += treat.velocity.x * frameRate * 100;
+            treat.position.y += treat.velocity.y * frameRate * 100;
+
+            treat.checkBounds();
+            treat.update();
+        },
+
+        checkBounds() {
+
+            if (treat.position.y > height - treat.radius) {
+                treat.velocity.y *= treat.restitution;
+                treat.position.y = height - treat.radius;
+            }
+            if (treat.position.x > width - treat.radius) {
+                treat.velocity.x *= treat.restitution;
+                treat.position.x = width - treat.radius;
+                treat.direction = -1;
+            }
+            if (treat.position.x < treat.radius) {
+                treat.velocity.x *= treat.restitution;
+                treat.position.x = treat.radius;
+                treat.direction = 1;
+            }
+
+        },
+
+        update() {
+            const relX = this.position.x - this.absolutePosition.x;
+            const relY = this.position.y - this.absolutePosition.y;
+
+            this.el.style.setProperty("--x", relX);
+            this.el.style.setProperty("--y", relY);
+            this.el.style.setProperty("--direction", this.direction);
+        }
+    };
+
+    setTimeout(() => {
+
+        function scoreFn(score) {
+            switch (score.getAttribute('name')) {
+                case "d": {
+                    score.style.fontSize = "7vmin"
+                    score.style.color = "#F44336";
+                } break;
+                case "e":
+                    {
+                        console.log('e', score);
+                        changeEnergyValue(parseInt(score.innerText));
+                    } break;
+                default: {
+                    score.style.fontSize = "15vmin";
+                    score.style.color = "#feca57";
+                } break;
+            }
+        }
+
+
+        const score = document.createElement('span');
+        score.setAttribute('name', treat.el.getAttribute('name'));
+        score.style.position = 'fixed';
+        score.style.top = treat.position.y;
+        score.style.left = treat.position.x;
+        score.innerText = treat.el.getAttribute('score');
+
+        scoreFn(score);
+        obj.elWrapper.appendChild(score);
+
+        treat.remove();
+
+
+        $(score).flyTo($('.treat-button'), $(score), function () {
+            // const anim = $('.treat-button').animate({ top: '-=2px' }, 40).animate({ top: '+=2px' }, 40).queue(function () {
+            //     $(score).remove();
+            //     $(this).dequeue();
+            // });
+            // $(anim).stop();
+        });
+
+
+    }, lifetime);
+    return treat;
+}
+
+
+function animationLoop() {
+    var i = treats.length;
+    while (i--) {
+        treats[i].animate();
+
+        if (!treats[i].animating) {
+            treats.splice(i, 1);
+        }
+
+        const treatsArr = document.getElementsByClassName('treat-wrapper')[0].getElementsByClassName('treat');        
+        if (treatsArr.length > 0) {
+            clearIntervalEnergy();
+        }
+        else {
+            if (ram.energyInterval == null) {
+                startIntervalEnergy();
+            }            
+        }
+    }
+    requestAnimationFrame(animationLoop);
+}
+
+animationLoop();
+
+function addTreats() {
+    changeEnergyValue(ram.energyClickValue, function (flag) {
+        if (flag) {
+            if (treats.length > 40) {
+                return;
+            }
+            for (let i = 0; i < 3; i++) {
+                treats.push(createTreat());
+            }
+        }
+    });
+    //cancelAnimationFrame(frame);
+
+}
